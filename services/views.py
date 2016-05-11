ab48a453f3e6f403cfb7ffc83f0876f82a4686e3
kamalteja <kamalteja1993@gmail.com>
2016-05-11 13:09:25 +0200

    Added DB insert functionality (just create new rows, every time page reloads)

--- services/views.py
+++ services/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import services #OR from services.models import services
 # Create your views here.
+import re
+import subprocess
 
 def services_create(request):
 	return HttpResponse("<h1>Create</h1>")
@@ -30,6 +32,18 @@ def services_list(request):		#Listing the services
 	# 	list_data = {
 	# 	"title": "Authentication Required"
 	# 	}
+	
+	service = subprocess.Popen(["service","--status-all"], stdout=subprocess.PIPE)
+	service_data = service.communicate()[0]
+	#print (service_data)
+
+
+	contentToArray=re.findall(r'\s*\[\s+(\+|\-)\s+\]\s+(.*)\s*', service_data)
+
+	#print (contentToArray)
+	for x in contentToArray:
+	 	print ("LINE: %s, %s" %(x[0],x[1]))
+	 	services.objects.create(service_name=x[1], current_status=x[0], prev_status=x[0])
 	service_list = services.objects.all()
 	list_data = {
 		"title": "List of Services",
