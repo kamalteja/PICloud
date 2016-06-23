c3fe0466539f42e777d8a31d660b5ad476a100b8
kamalteja <kamalteja1993@gmail.com>
2016-06-23 14:27:46 +0200

    styling suthentication page

--- filesCDN/js/general.js
+++ filesCDN/js/general.js
@@ -2,6 +2,10 @@ function load() {
 	element=document.getElementsByClassName("unloader")[0];
     element.className="loader";
 }
+function load_list() {
+	element=document.getElementsByClassName("unloader-list")[0];
+    element.className="loader";
+}
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
